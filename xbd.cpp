fa00b6423c671205992ab4abc47e016e03b399cd
Blake Meyers <41344224+bmeyers202@users.noreply.github.com>
2019-02-17 15:23:01 -0800

    Update xbd.cpp

--- xbd.cpp
+++ xbd.cpp
@@ -19,60 +19,54 @@ using namespace std;
  * and calls the appropriate function for the output format.
  */
 int main (int argc, char *argv[]) {
-    
+
     if(strcmp(argv[1], "-b") == 0){
         handleBinary(argv[2]);
     }
     else{
         handleHex(argv[1]);
     }
-    
+
     return 0;
 }
 
 /*
  * Converts the file contents into hex format
  */
-void handleHex (char file_in_name[]) {
-    long int addr;
+void handleHex (char fileName[]) {
     unsigned char buffer[100];
     long int bufferCnt;
     long int indexCnt;
-    ifstream file_in;
+    long int address;
+    ifstream fileIn;
     long index;
-    //
+
     //  Open the file.
-    //
-    file_in.open (file_in_name);
-    
-    if ( !file_in ) {
-        cout << "\n";
-        cout << "Cannot open \"" << file_in_name << "\"\n";
+    fileIn.open (fileName);
+
+    if (!fileIn) {
+        cout << endl;
+        cout << "Cannot open \"" << fileName << "\"" << endl;
         return;
     }
-    
-    //
+
     //  Dump the file contents.
-    //
-    addr = 0;
-    
-    while (1) {
-        file_in.read ((char *) buffer, 16 );
-        
-        bufferCnt = file_in.gcount();
-        
+    address = 0;
+
+    while(cin.good()) {
+        fileIn.read ((char *) buffer, 16 );
+
+        bufferCnt = fileIn.gcount();
+
         if (bufferCnt <= 0) {
             break;
         }
-        //
+
         //  Prints the address in hexadecimal.
-        //
-        cout << hex << setw(7) << setfill('0') << (int) addr << "  ";
-        
-        addr = addr + 16;
-        //
-        //  Print 16 data items in hexadecimal with 8 bits in one column.
-        //
+        cout << hex << setw(7) << setfill('0') << (int) address << "  ";
+        address = address + 16;
+
+        //  Print 16 data items in hexadecimal with 16 bits in one column.
         indexCnt = 0;
         for (index = 0; index < 16; index++) {
             indexCnt = indexCnt + 1;
@@ -85,11 +79,10 @@ void handleHex (char file_in_name[]) {
             if(indexCnt % 2 ==0)
                 cout << " ";
         }
-        
+
         cout << setfill (' ');
-        //
+
         //  Prints printable characters or a period otherwise.
-        //
         cout << " ";
         indexCnt = 0;
         for (index = 0; index < 16; index++) {
@@ -103,65 +96,53 @@ void handleHex (char file_in_name[]) {
                 }
             }
         }
-        cout << "\n";
+        cout << endl;
         cout << dec;
-        
-        if (file_in.eof( )) {
-            break;
-        }
-        
     }
-    //
-    // Close the file.
-    //
-    file_in.close( );
-    
+
+    // Close the file
+    fileIn.close( );
+
     return;
 }
 
 /*
  * Converts the file contents into binary format
  */
-void handleBinary (char file_in_name[]) {
-    long int addr;
+void handleBinary (char fileName[]) {
+    long int address;
     unsigned char buffer[100];
     long int bufferCnt;
     long int indexCnt;
-    ifstream file_in;
+    ifstream fileIn;
     long index;
-    //
+
     //  Open the file.
-    //
-    file_in.open (file_in_name);
-    
-    if (!file_in) {
-        cout << "\n";
-        cout << "Cannot open \"" << file_in_name << "\"\n";
+    fileIn.open (fileName);
+
+    if (!fileIn) {
+        cout << endl;
+        cout << "Cannot open \"" << fileName << "\"" << endl;
         return;
     }
-    
-    //
-    //  Dump the file contents.
-    //
-    addr = 0;
-    
-    while (1) {
-        file_in.read ((char *) buffer, 6 );
-        
-        bufferCnt = file_in.gcount();
-        
+
+    //Dump the file contents.
+    address = 0;
+
+    while(cin.good()) {
+        fileIn.read ((char *) buffer, 6 );
+
+        bufferCnt = fileIn.gcount();
+
         if (bufferCnt <= 0) {
             break;
         }
-        //
-        //  Prints the address in Hex.
-        //
-        cout << hex << setw(7) << setfill( '0') << (int) addr << "  ";
-        
-        addr = addr + 6;
-        //
-        //  Print 6 data items in binary with 8 bits each.
-        //
+
+        //Prints the address in Hex.
+        cout << hex << setw(7) << setfill( '0') << (int) address << "  ";
+        address = address + 6;
+
+        //Print 6 data items in binary with 8 bits each.
         indexCnt = 0;
         for (index = 0; index < 6; index++) {
             indexCnt = indexCnt + 1;
@@ -173,11 +154,10 @@ void handleBinary (char file_in_name[]) {
             }
             cout << " ";
         }
-        
+
         cout << setfill (' ');
-        //
+
         //  Prints printable characters or a period otherwise.
-        //
         cout << " ";
         indexCnt = 0;
         for (index = 0; index < 6; index++) {
@@ -191,18 +171,12 @@ void handleBinary (char file_in_name[]) {
                 }
             }
         }
-        cout << "\n";
+        cout << endl;
         cout << dec;
-        
-        if (file_in.eof( )) {
-            break;
-        }
-        
     }
-    //
+
     // Close the file.
-    //
-    file_in.close( );
-    
+    fileIn.close( );
+
     return;
 }

9e06807ee274e378e75fe0373b0b93a95fbd042a
Blake Meyers <41344224+bmeyers202@users.noreply.github.com>
2019-02-17 16:53:18 -0800

    Update xbd.cpp

--- xbd.cpp
+++ xbd.cpp
@@ -1,14 +1,11 @@
-/**
- *  Program #1
- *  A basic hexdump program that opens a binary or text file, read every byte in
- *  the file and write both the ASCII hex value for that byte as well as it’s printable
- *  character to standard output. For bytes forming non-printable characters, prints a ‘.’
- *  character
- *  CS 530 - 1
- *  2/17/19
- *  @author Blake Meyers cssc0430
- *  @author Rahul Sharma cssc0417
- */
+/** File: xbd.cpp
+  * Names: Blake Meyers, Rahul Sharma
+  * Class account username: Blake Meyers cssc0430,  Rahul Sharma cssc0417
+  * Class information: CS530 Spring 2019
+  * Assignment information: Assignment #1, HexDump. A basic hexdump program that opens a binary or text file, reads every byte in
+  * the file and writes both the ASCII and hex value or Binary value for that byte as well as it’s printable
+  * character to standard output. For bytes forming non-printable characters, prints a ‘.’character
+*/
 
 #include "xbd.h"
 

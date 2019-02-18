c79aee554956325fd779e58c56bbbcb0d25051da
srahull <41943560+srahull@users.noreply.github.com>
2019-02-17 16:53:43 -0800

    Update xbd.h

--- xbd.h
+++ xbd.h
@@ -1,3 +1,14 @@
+/*
+ *Names: Rahul Sharma, Blake Meyers
+ *Class account username: Blake Meyers cssc0430,  Rahul Sharma cssc0417
+ *Class information: CS530 Spring 2019
+ *Assignment information: Assignment #1, HexDump. A basic hexdump program that opens a binary or text file, reads every byte in
+ *the file and writes both the ASCII and hex value or Binary value for that byte as well as it’s printable
+ *character to standard output. For bytes forming non-printable characters, prints a ‘.’character
+ *
+ */
+
+
 #ifndef XBD_H_
 #define XBD_H_
 # include <cstdlib>
